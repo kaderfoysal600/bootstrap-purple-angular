a5205da822b243114a0dce6ca71f6a92486823f1
kader foysal <kaderf113@gmail.com>
2023-06-04 20:36:45 +0600

    remove node-sass

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,53 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 // import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
   // providers: [NgbDropdownConfig]
 })
 export class NavbarComponent implements OnInit {
   public iconOnlyToggled = false;
   public sidebarToggled = false;
-  
-  constructor(
+  public isDropdownOpen = false;
+  public isDropdownOpen2 = false;
+  public isDropdownOpen3 = false;
+  constructor() {
     // config: NgbDropdownConfig
-    ) {
     // config.placement = 'bottom-right';
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // toggle sidebar in small devices
   toggleOffcanvas() {
-    document.querySelector('.sidebar-offcanvas').classList.toggle('active');
+    document.querySelector(".sidebar-offcanvas").classList.toggle("active");
   }
 
   // toggle sidebar
   toggleSidebar() {
-    let body = document.querySelector('body');
-    if((!body.classList.contains('sidebar-toggle-display')) && (!body.classList.contains('sidebar-absolute'))) {
+    let body = document.querySelector("body");
+    if (
+      !body.classList.contains("sidebar-toggle-display") &&
+      !body.classList.contains("sidebar-absolute")
+    ) {
       this.iconOnlyToggled = !this.iconOnlyToggled;
-      if(this.iconOnlyToggled) {
-        body.classList.add('sidebar-icon-only');
+      if (this.iconOnlyToggled) {
+        body.classList.add("sidebar-icon-only");
       } else {
-        body.classList.remove('sidebar-icon-only');
+        body.classList.remove("sidebar-icon-only");
       }
     } else {
       this.sidebarToggled = !this.sidebarToggled;
-      if(this.sidebarToggled) {
-        body.classList.add('sidebar-hidden');
+      if (this.sidebarToggled) {
+        body.classList.add("sidebar-hidden");
       } else {
-        body.classList.remove('sidebar-hidden');
+        body.classList.remove("sidebar-hidden");
       }
     }
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    this.isDropdownOpen2 = false;
+    this.isDropdownOpen3 = false;
+    console.log("drop1 clicked");
+  }
+  toggleDropdown2() {
+    this.isDropdownOpen2 = !this.isDropdownOpen2;
+    this.isDropdownOpen = false;
+    this.isDropdownOpen3 = false;
+    console.log("drop2 clicked");
+  }
+  toggleDropdown3() {
+    this.isDropdownOpen3 = !this.isDropdownOpen3;
+    this.isDropdownOpen = false;
+    this.isDropdownOpen2 = false;
+    console.log("drop3 clicked");
   }
 
   // toggle right sidebar
   // toggleRightSidebar() {
   //   document.querySelector('#right-sidebar').classList.toggle('open');
   // }
-
 }
